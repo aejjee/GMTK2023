16cb440238f14e61ce456393f53f15acec894147
BagelSeasoning234 <70250943+BagelSeasoning234@users.noreply.github.com>
2023-07-07 21:47:35 -0700

    Sam's towers

--- Source/GMTK2023/GMTK2023.Build.cs
+++ Source/GMTK2023/GMTK2023.Build.cs
@@ -8,7 +8,7 @@ public class GMTK2023 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Paper2D" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
