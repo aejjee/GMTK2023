ae2e77e1f55e9f3c548041f226bd2f0fc3b4b111
Aejjee <mrpapeerclip21@gmail.com>
2023-07-07 17:21:17 -0700

    troop tags

--- Source/GMTK2023/TroopBase.cpp
+++ Source/GMTK2023/TroopBase.cpp
@@ -11,7 +11,7 @@ ATroopBase::ATroopBase()
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	
+	Tags.Add("troop");
 
 	health = 100.0f;
 }
